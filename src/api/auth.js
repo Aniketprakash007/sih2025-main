792403f338155e599c87fcbda03e0ba73faa9ce8
Aniket Prakash <140113260+Aniketprakash007@users.noreply.github.com>
2025-10-22 21:49:04 +0530

    Update API_URL to use production endpoint

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_URL =
+  process.env.REACT_APP_API_URL || "https://auth-backend-g59s.onrender.com";
+
 
 // Signup user with all fields
 export async function signupUser({ name, school, grade, location, email, password }) {
